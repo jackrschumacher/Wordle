b4314a3756b9a4f8258a1cbeb95ef079854a0430
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-14 17:06:40 +0000

    Add Words to ArrayList

--- Main.java
+++ Main.java
@@ -3,5 +3,10 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
+
+
+    ArrayList<Sring> WORD_LIST = new ArrayList<String>(){"zesty", "zebra", "zippy", "zonal", "zones","wages", "wains", "wakes", "wales", "walks","xenon", "xylem", "xylyl", "xebec", "xrays","fancy", "felon", "fever", "fight", "final","rabbi", "roose", "resin", "revel", "rater","eager", "early", "earth", "elbow", "elect","udder","under","uncle","union","unity","organ", "onion ", "older",  "ocean ", "olive ", "pedal ", " pixel", "plain", "plane", "penny", "panic","hydro", "helix", "harpy", "heist", "horde","jolly", "juror", "joker", "jumpy", "judge","makes", "macaw", "macro", "micro", "magic","nacho", "ninja", "navvy", "nymph", "niece","start", "shown", "scrap", "stare", "smear","train", "track", "timed", "thyme", "tours","knead", "kites", "kilos", "knelt", "knoll", "label", "labor", "lacks", "lapel", "lamas","icing", "igloo", "index", "inter", "irony","gamer", "golem", "greed", "ghoul", "gamma","dough", "dream", "doubt", "death", "darts","bacon", "basic", "beach", "beard", "beats","quasi", "quail", "quads", "quack", "queen","apple", "adult", "asset", "above", "angry","court", "cases", "check", "chick", "chain","yoink", "yearn", "young", "youth", "yacht","video", "valve", "vapor", "valid", "valet"
+}
+    
   }
 }
